2ad81f61a701222e0632c8d4e706a99feb06651c
John Rix <john@johnrix.com>
2018-01-16 16:34:19 +0000

    Removed unintended iupdate to ng-click expression

--- src/scripts/mdSidemenuContent/template.js
+++ src/scripts/mdSidemenuContent/template.js
@@ -2,7 +2,7 @@ export default function() {
 
   return `
     <div class="md-sidemenu-content" layout="column">
-      <md-button class="md-sidemenu-toggle" ng-if="$mdSidemenuContent.heading" ng-click="!$mdSidemenuContent.onHover && $mdSidemenuContent.changeState();" ng-class="{ 'md-active': $mdSidemenuContent.visible }">
+      <md-button class="md-sidemenu-toggle" ng-if="$mdSidemenuContent.heading" ng-click="$mdSidemenuContent.changeState();" ng-class="{ 'md-active': $mdSidemenuContent.visible }">
         <div layout="row">
           <md-icon ng-if="$mdSidemenuContent.svgIcon" md-svg-icon="{{$mdSidemenuContent.svgIcon}}"></md-icon>
           <md-icon ng-if="$mdSidemenuContent.icon">{{ $mdSidemenuContent.icon }}</md-icon>
